ba627e4a1516377c45c6fe90ea0170a2cc2c5b24
Nochini <93994299+Nochini@users.noreply.github.com>
2022-04-04 13:57:02 -0300

    04/04/22
    Updates

---
+++ script.js
@@ -0,0 +1,10 @@
+//Javascript do toggle button
+
+const toggleButton = document.getElementsByClassName('toggle-btn')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+// Animação TypeWritter para introdução do nível
